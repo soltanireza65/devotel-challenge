f7bf7655c380f0df8066930065432aa5ccee6fa9
Reza Soltani <soltanireza65@gmail.com>
2025-02-09 09:52:02 -0800

    chore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JobOffersModule } from './job-offers/job-offers.module';
 
 @Module({
   imports: [
+    JobOffersModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
