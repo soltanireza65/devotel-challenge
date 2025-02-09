5e715344eb03a1df9f56e630e6a0adba84e9e58e
Reza Soltani <soltanireza65@gmail.com>
2025-02-09 11:00:16 -0800

    chore

--- src/shared/env/env.ts
+++ src/shared/env/env.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod';
 
 export const envSchema = z.object({
+  COMPOSE_FILE: z.string(),
+  
   ENV: z.enum(['development', 'production']),
   HTTP_PORT: z.coerce.number().optional().default(3000),
   JOB_OFFER_CRON_SCHEDULE: z.string(),
+
   REDIS_HOST: z.string(),
   REDIS_PORT: z.coerce.number(),
   REDIS_PASSWORD: z.string(),
